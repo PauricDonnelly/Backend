f18d29f1aaca5c331f0ed401031c954eeaa57ed1
Pauric Donnelly <pauric.donnelly@kainos.com>
2022-09-15 14:03:09 +0100

    Adding new constructor

--- JavaWebServiceGroupB/src/main/java/com/kainos/ea/employee_stuff/Employee.java
+++ JavaWebServiceGroupB/src/main/java/com/kainos/ea/employee_stuff/Employee.java
@@ -168,5 +168,19 @@ public class Employee implements Payable, Comparable<Employee> {
         this.email = email;
     }
 
+    @JsonCreator
+    public Employee(@JsonProperty("salary") double salary,
+                     @JsonProperty("firstname") String firstName, @JsonProperty("lastname") String lastName,
+                     @JsonProperty("bankAccountNumber") String bankAccountNumber, @JsonProperty("niNumber") String niNumber,
+                     @JsonProperty("phoneNumber") String phoneNumber, @JsonProperty("email") String email) {
+        this.salary = salary;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.bankAccountNumber = bankAccountNumber;
+        this.niNumber = niNumber;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
 
 }
